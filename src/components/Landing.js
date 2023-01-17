eaf168dcab458907731ffa91234b424fb2c93f61
Hadi <hadirezaei2828@gmail.com>
2023-01-17 14:04:30 +0330

    add Loader

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getCoin } from '../services/api';
+import Loader from './Loader';
 
 const Landing = () => {
+
+    const [coins, setCoins] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => {
             const data = await getCoin();
-            console.log(data);
+            setCoins(data);
         }
 
         fetchAPI()
@@ -14,7 +17,17 @@ const Landing = () => {
 
     return (
         <div>
-            
+            <input type="text" placeholder="Search" />
+            {
+                coins.length ?
+                    <div>
+                        {
+                            coins.map(coin => <p key={coin.id}>{coin.name}</p>)
+                        }
+                    </div> :
+                    <Loader />
+            }
+
         </div>
     );
 };
